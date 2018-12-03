54aa89f65a4a858a2533d0468ef6a5ac234221d6
Rishabh Kashyap <kashyap.rishab91@gmail.com>
2018-12-03 18:46:58 +0530

    Removing unused code to calculate size of weigh list

--- perceptron/com/algo/perceptron/perceptron.py
+++ perceptron/com/algo/perceptron/perceptron.py
@@ -10,28 +10,22 @@ class Perceptron:
         self.errors = []
 
     def train(self, X, y):
-        weight_len = X.shape[1]+1
-        self.weights=np.zeros(1 + X.shape[1])
-        delta=None
 
-        # for i in range(weight_len):
-        #     self.weights.append(0)
+        self.weights = np.zeros(1 + X.shape[1])
+        delta = None
 
         for _ in range(self.iteration):
-            delta=0.0
+            delta = 0.0
             for xi, target in zip(X, y):
-                error = self.learning_rate*(target - self.predict(xi))
-                self.weights[1:] += error *xi
+                error = self.learning_rate * (target - self.predict(xi))
+                self.weights[1:] += error * xi
                 self.weights[0] += error * self.learning_rate
-                delta+=int(error!=0.0)
+                delta += int(error != 0.0)
             self.errors.append(delta)
-
 
     def predict(self, X):
         return np.where(self.net_input(X) >= 0, 1, -1)
 
-
     def net_input(self, X):
 
         return np.dot(X, self.weights[1:]) + self.weights[0]
-
